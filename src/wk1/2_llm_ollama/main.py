e58194af9019369f55bc34f645f6994513087530
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-05-22 22:50:44 +0100

    refactored code

--- src/wk1/2_llm_ollama/main.py
+++ src/wk1/2_llm_ollama/main.py
@@ -8,3 +8,15 @@ from IPython.display import Markdown, display
 OLLAMA_API = "http://localhost:11434/api/chat"
 HEADERS = {"Content-Type": "application/json"}
 MODEL = "llama3.2"
+
+messages = [
+    {"role": "user", "content": "Describe some the business applications of Generative AI."},
+]
+
+payload = {
+    "model": MODEL,
+    "messages": messages,
+    "stream": False
+}
+
+response = requests.post(OLLAMA_API, json=payload, headers=HEADERS)
