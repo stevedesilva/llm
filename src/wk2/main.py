fd396277abff01ed4855ae5e2a04ab54018353b7
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-07-02 00:10:47 +0100

    setup keys

---
+++ src/wk2/main.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import anthropic
+# from IPython import display, Markdown, update_display
+
+import google.generativeai
+
+
+load_dotenv(override=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+google_api_key = os.getenv('GOOGLE_API_KEY')
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+
+if anthropic_api_key:
+    print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+else:
+    print("Anthropic API Key not set")
+
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:8]}")
+else:
+    print("Google API Key not set")
