d9d3907ac212f1c7d888bb62b7a30713eb774eb6
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-07-02 22:14:00 +0100

    add call to apenai

--- src/wk2/main.py
+++ src/wk2/main.py
@@ -25,3 +25,22 @@ if google_api_key:
     print(f"Google API Key exists and begins {google_api_key[:8]}")
 else:
     print("Google API Key not set")
+
+# Initialize clients
+openai = OpenAI()
+
+claude = anthropic.Anthropic()
+
+google.generativeai.configure()
+
+system_message = "You are an assistant that is great at telling jokes"
+# user_prompt = "Tell a light-hearted joke for an audience of Data Scientists"
+user_prompt = "Tell a light-hearted joke for an audience of Computer Scientists"
+
+prompts = [
+    {"role": "system", "content": system_message},
+    {"role": "user", "content": user_prompt}
+]
+
+completion = openai.chat.completions.create(model='gpt-4o-mini', messages=prompts)
+print(completion.choices[0].message.content)
