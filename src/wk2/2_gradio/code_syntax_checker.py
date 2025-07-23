7b1a8ed09cbb17e01ed77e26850511cdb5352386
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-07-23 08:18:02 +0100

    code checker init

---
+++ src/wk2/2_gradio/code_syntax_checker.py
@@ -0,0 +1,73 @@
+import os
+import string
+
+from dotenv import load_dotenv
+from openai import OpenAI
+import anthropic
+
+import gradio as gr
+
+load_dotenv(override=True)
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+
+# anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+# if anthropic_api_key:
+#     print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+# else:
+#     print("Anthropic API Key not set")
+#
+# google_api_key = os.getenv('GOOGLE_API_KEY')
+# if google_api_key:
+#     print(f"Google API Key exists and begins {google_api_key[:8]}")
+# else:
+#     print("Google API Key not set")
+
+
+# Initialize clients
+
+openai = OpenAI()
+# claude = anthropic.Anthropic()
+# gemini_via_openai_client = OpenAI(
+#     api_key=google_api_key,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+# )
+
+# Let's make a conversation between GPT-4o-mini and Claude-3-haiku
+# We're using cheap versions of models so the costs will be minimal
+
+
+gpt_model = "gpt-4o-mini"
+# claude_model = "claude-3-haiku-20240307"
+# gemini_model = "gemini-2.0-flash-lite"
+
+system_message = ("You are a helpful coding instructor. You help students learn python, java and golang. \
+You ask syntax questions for a selected language and then review the result\
+You give the student a score and some helpful advice to improve")
+
+# get syntax question to ask student for a given language
+def get_question(language):
+    return ""
+
+def ask_question(language):
+    question = get_question(language)
+    return question
+
+def check_answer():
+    # This function will check the answer given by the student
+    # It will return a score and some advice
+    return 0, "Good job, but you could improve by..."
+
+
+# normal launch
+# gr.Interface(fn=shout,inputs="textbox", outputs="textbox", flagging_mode="never").launch()
+
+# launch in browser
+# gr.Interface(fn=shout,inputs="textbox", outputs="textbox", flagging_mode="never").launch(inbrowser=True)
+
+# share public link
+gr.Interface(fn=shout,inputs="textbox", outputs="textbox", flagging_mode="never").launch(share=True)
