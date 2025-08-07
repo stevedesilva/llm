7d918d9d1b0e36a5b21d330edd41bef2de5381cf
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-08-07 21:13:34 +0100

    show image

--- src/wk2/5_airline_assistant/main.py
+++ src/wk2/5_airline_assistant/main.py
@@ -83,10 +83,19 @@ def chat(message, history):
 
 
 
-gr.ChatInterface(fn=chat,type="messages").launch()
+# gr.ChatInterface(fn=chat,type="messages").launch()
 
+def artist(city: str):
+    image_response = openai.images.generate(
+         model="dall-e-3",
+         prompt=f"An image representing a vacation in {city}, showing tourist spots and everything unique about {city}, in a vibrant pop-art style",
+         size="1024x1024",
+         n=1,
+         response_format="b64_json"
+    )
+    image_base64 = image_response.data[0].b64_json
+    image_data = base64.b64decode(image_base64)
+    return Image.open(BytesIO(image_data))
 
-
-
-
-
+image = artist("London")
+image.show()
